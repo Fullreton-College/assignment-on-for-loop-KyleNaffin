1b797109cfcd7fb81f57ca2b40b116e198388a78
Naffin, Kyle <kdnaffin@gmail.com>
2026-02-24 20:00:15 -0800

    Finished Assignment

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
 
-   /* Type your code here. */
+   int sale1, sale2, sale3, sale4, sale5;
+   cout<<"Enter today's sales for store 1: ";
+   cin>>sale1;
+   cout<<"Enter today's sales for store 2: ";
+   cin>>sale2;
+   cout<<"Enter today's sales for store 3: ";
+   cin>>sale3;
+   cout<<"Enter today's sales for store 4: ";
+   cin>>sale4;
+   cout<<"Enter today's sales for store 5: ";
+   cin>>sale5;
+
+   cout<<"==============="<<endl;
+   cout<<"SALES BAR CHART"<<endl;
+   cout<<"(Each * = $100)"<<endl;
+
+   cout<<"Store 1: ";
+   for (int i = 0; i < sale1; i+=100){
+      cout<<"*";
+   }
+   cout<<endl;
+   
+   cout<<"Store 2: ";
+   for (int i = 0; i < sale2; i+=100){
+      cout<<"*";
+   }
+   cout<<endl;
+
+   cout<<"Store 3: ";
+   for (int i = 0; i < sale3; i+=100){
+      cout<<"*";
+   }
+   cout<<endl;
+
+   cout<<"Store 4: ";
+   for (int i = 0; i < sale4; i+=100){
+      cout<<"*";
+   }
+   cout<<endl;
+
+   cout<<"Store 5: ";
+   for (int i = 0; i < sale5; i+=100){
+      cout<<"*";
+   }
+   cout<<endl;
 
    return 0;
 }
--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -1,10 +1,25 @@
-// Chapter 5, Programming Challenge 17: Sales Bar Chart
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main()
 {
+    int start, end;
+    cout<<"Enter first integer: ";
+    cin>>start;
 
-return 0;
+    cout<<"Enter the second integer: ";
+    cin>>end;
+
+    while (end < start){
+        cout<<"Second integer must be larger than the first. Try again..."<<endl;
+        cin>>end;
+    }
+
+    for (int i = start; i <= end; i+=5){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    return 0;
 }
